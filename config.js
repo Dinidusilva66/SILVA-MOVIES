d0c682ee229f001f7fd1d115e2ce48c74856024f
Dinidusilva66 <118092626+Dinidusilva66@users.noreply.github.com>
2025-01-31 17:19:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'l0RmULaK#xxvpZc2q_msr3ON26RPHfYKw643auax5OPEsG30Bi0o',
-GITHUB_AUTH_TOKEN: 'put token', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'put your github user name',
+GITHUB_AUTH_TOKEN: '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', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'Dinidusilva66',
 
 };
